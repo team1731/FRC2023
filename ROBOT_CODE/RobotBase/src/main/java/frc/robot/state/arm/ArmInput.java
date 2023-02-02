503cc68b2bc16d6cfcfeb676959dc25e6a8b1a21
Margot Byrum <twainandtea@gmail.com>
2023-02-02 18:57:55 -0500

    Adding additional states for managing arm motion

--- ROBOT_CODE/RobotBase/src/main/java/frc/robot/state/arm/ArmInput.java
+++ ROBOT_CODE/RobotBase/src/main/java/frc/robot/state/arm/ArmInput.java
@@ -6,7 +6,8 @@ import frc.robot.state.*;
  * Defines valid inputs for state transitions
  */
 public enum ArmInput implements Input {
-    EXTEND, RETRACT, INTAKE, RELEASE, RECOVER, INTERRUPT, SUCCESS, FAILED;
+    EXTEND_INIT, EXTEND_MOVE, EXTEND_PING, INTAKE, RELEASE, RETRACT_INIT, 
+    RETRACT_MOVE, RETRACT_PING, RECOVER, INTERRUPT, SUCCESS, FAILED;
 
     public String getDescription() {
         return "ArmStateInput: " + this.toString();
