dc34a1780f5924ac6e3a8b56113078d5b756b0ff
david <davidanandsamy@aol.com>
2023-02-09 19:32:45 -0500

    Fixing ArbitraryFeedForward function for degrees

--- ROBOT_CODE/AlexanderGCowbell/src/main/java/frc/robot/util/ArbitraryFeedForward.java
+++ ROBOT_CODE/AlexanderGCowbell/src/main/java/frc/robot/util/ArbitraryFeedForward.java
@@ -5,14 +5,14 @@ import frc.robot.Constants.ArmConstants;
 public class ArbitraryFeedForward {
     public static double armExtension(double proximalTicks, double distalTicks) {
         //Calculates how far the arm is extended using trigonometry and angles derived from data from motors 
-        double distalDistance = (ArmConstants.distalArmLength * java.lang.Math.cos(3)) * java.lang.Math.sin(distalTicks * ArmConstants.distalTicksPerDegree); 
-        double proximalDistance = ArmConstants.proximalArmLength * java.lang.Math.sin(proximalTicks * ArmConstants.proximalTicksPerDegree);
+        double distalDistance = (ArmConstants.distalArmLength * java.lang.Math.cos(Math.toRadians(3)) * java.lang.Math.sin(Math.toRadians(distalTicks * ArmConstants.distalTicksPerDegree))); 
+        double proximalDistance = ArmConstants.proximalArmLength * java.lang.Math.sin(Math.toRadians(proximalTicks * ArmConstants.proximalTicksPerDegree));
         return distalDistance + proximalDistance; 
     }
     
     public static double distalArmExtension(double distalTicks){
         //Calculates how far the arm is extended using trigonometry and angles derived from data from motors 
-        double distalDistance = (ArmConstants.distalArmLength * java.lang.Math.cos(3)) * java.lang.Math.sin(distalTicks * ArmConstants.distalTicksPerDegree); 
+        double distalDistance = (ArmConstants.distalArmLength * java.lang.Math.cos(Math.toRadians(3))) * java.lang.Math.sin(Math.toRadians(distalTicks * ArmConstants.distalTicksPerDegree)); 
         return distalDistance; 
     }
 
