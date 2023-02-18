3e51a70f62d3e36db8a34b29dec5deea0b85b03f
Margot Byrum <twainandtea@gmail.com>
2023-02-18 14:34:44 -0500

    updated ArmStateTest (initialize)

--- ROBOT_CODE/AlexanderGCowbell/src/test/java/frc/robot/state/arm/ArmStateTest.java
+++ ROBOT_CODE/AlexanderGCowbell/src/test/java/frc/robot/state/arm/ArmStateTest.java
@@ -30,6 +30,35 @@ public class ArmStateTest {
       assertEquals(ArmState.HOME, state, "Didn't move to HOME");
     } finally {}
   }
+  
+  @Test 
+  void successfulArmInitializeTransitions() {
+    ArmState state = ArmState.UNKNOWN;
+    try {
+      state = (ArmState)state.next(Input.INITIALIZE);
+      assertEquals(ArmState.RESETTING_WRIST, state, "Didn't move to RESETTING_WRIST");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.RESETTING_PROXIMAL, state, "Didn't move to RESETTING_PROXIMAL");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.RESETTING_DISTAL, state, "Didn't move to RESETTING_DISTAL");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.HOME, state, "Didn't move to HOME");
+      state = (ArmState)state.next(Input.EXTEND);
+      assertEquals(ArmState.EXTENDING, state, "Didn't move to EXTENDING");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.EXTENDED, state, "Didn't move to EXTENDED");
+      state = (ArmState)state.next(Input.RETRACT);
+      assertEquals(ArmState.RETRACTING, state, "Didn't move to RETRACTING");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.RESETTING_WRIST, state, "Didn't move to RESETTING_WRIST");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.RESETTING_PROXIMAL, state, "Didn't move to RESETTING_PROXIMAL");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.RESETTING_DISTAL, state, "Didn't move to RESETTING_DISTAL");
+      state = (ArmState)state.next(Input.COMPLETED);
+      assertEquals(ArmState.HOME, state, "Didn't move to HOME");
+    } finally {}
+  }
 
   @Test 
   void successfulIntakeTransitions() {
