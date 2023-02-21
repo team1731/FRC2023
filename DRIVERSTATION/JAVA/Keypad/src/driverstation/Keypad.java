4d1e3f469178de0dde1057077c09ccf9a12c152c
daniel <rdanielbuckley@gmail.com>
2023-02-20 22:45:38 -0500

    add error-checking to keypad; improve logic

--- DRIVERSTATION/JAVA/Keypad/src/driverstation/Keypad.java
+++ DRIVERSTATION/JAVA/Keypad/src/driverstation/Keypad.java
@@ -1,6 +1,7 @@
 package driverstation;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -19,7 +20,8 @@ import edu.wpi.first.networktables.NetworkTableValue;
 
 public class Keypad extends JDialog implements KeyListener {
 	private static final long serialVersionUID = 1L;
-	private JLabel label;
+	private JLabel commandLabel;
+	private JLabel feedbackLabel;
 	private JLabel picLabel;
     private NetworkTable table;
     private State state;
@@ -29,8 +31,6 @@ public class Keypad extends JDialog implements KeyListener {
     private ImageIcon cone;
     private ImageIcon none;
     private ImageIcon lock;
-    
-    private boolean NUMLOCK = false;
     
 	public Keypad() {
 		cone = new ImageIcon(Keypad.class.getResource("/images/cone.png"));
@@ -42,9 +42,14 @@ public class Keypad extends JDialog implements KeyListener {
 	    inst.setServerTeam(1731);
 	    inst.startDSClient();
 		table = inst.getTable("KeyPad");
-		label = new JLabel();
-		label.setFont(new Font("Arial Black", Font.BOLD, 32));
-		add(label, BorderLayout.SOUTH);
+		feedbackLabel = new JLabel();
+		feedbackLabel.setFont(new Font("Arial Black", Font.BOLD, 24));
+		feedbackLabel.setForeground(Color.RED);
+		add(feedbackLabel, BorderLayout.NORTH);
+		commandLabel = new JLabel();
+		commandLabel.setFont(new Font("Arial Black", Font.BOLD, 32));
+		commandLabel.setForeground(Color.BLUE);
+		add(commandLabel, BorderLayout.SOUTH);
 		picLabel = new JLabel(none);
 		add(picLabel, BorderLayout.CENTER);
 		state = State.INPUT;
@@ -67,15 +72,23 @@ public class Keypad extends JDialog implements KeyListener {
     }
 
     enum KeypadControl {
-    	GET_CONE    (111), // "/"
-    	GET_CUBE    (106), // "*"
-    	CONE_LEFT   (100), // "4"
-    	CUBE_MIDDLE (101), // "5"
-    	CONE_RIGHT  (102), // "6"
-    	DEPLOY_LOW   (97), // "1"
-    	DEPLOY_MID   (98), // "2"
-    	DEPLOY_HIGH  (99), // "3"
-    	CLEAR_ENTRY  (96); // "0"
+    	CONE        (111), // "/"
+    	CUBE        (106), // "*"
+    	SPARE_A       (8), // "<--"
+    	GRID_1      (103), // "7"
+    	GRID_2      (104), // "8"
+    	GRID_3      (105), // "9"
+    	SPARE_B     (109), // "-"
+    	LEFT        (100), // "4"
+    	MIDDLE      (101), // "5"
+    	RIGHT       (102), // "6"
+    	SPARE_C     (107), // "+"
+    	SCORE_LOW    (97), // "1"
+    	SCORE_MED    (98), // "2"
+    	SCORE_HIGH   (99), // "3"
+    	SPARE_D      (10), // "Ent"
+    	CLEAR_ENTRY  (96), // "0"
+    	SPARE_E     (110); // "."
     	
     	private final int value;
     	private static Map<Integer, KeypadControl> map = new HashMap<>();
@@ -101,21 +114,26 @@ public class Keypad extends JDialog implements KeyListener {
     
     enum State {
         INPUT,
-        PENDING;
+        PENDING1,
+        PENDING2,
+    	DONE
     }
 
     Object stateTransitionTable[][] = {
-    	// CURRENT      INPUT                     OPERATION            ICON    NEXT
-        {State.INPUT,   KeypadControl.GET_CONE,   "lightUpBot",        cone,   State.INPUT},
-        {State.INPUT,   KeypadControl.GET_CUBE,   "lightUpBot",        cube,   State.INPUT},
-        {State.INPUT,   KeypadControl.CONE_LEFT,   null,               cone,   State.PENDING},
-        {State.INPUT,   KeypadControl.CUBE_MIDDLE, null,               cube,   State.PENDING},
-        {State.INPUT,   KeypadControl.CONE_RIGHT,  null,               cone,   State.PENDING},
-        {State.INPUT,   KeypadControl.CLEAR_ENTRY, "clearInputBuffer", none,   State.INPUT},
-        {State.PENDING, KeypadControl.CLEAR_ENTRY, "clearInputBuffer", none,   State.INPUT},
-        {State.PENDING, KeypadControl.DEPLOY_LOW,  "armJoystick",      null,   State.INPUT},
-        {State.PENDING, KeypadControl.DEPLOY_MID,  "armJoystick",      null,   State.INPUT},
-        {State.PENDING, KeypadControl.DEPLOY_HIGH, "armJoystick",      null,   State.INPUT},
+    	// CURRENT         INPUT                     OPERATION            ICON    NEXT
+        {State.INPUT,      KeypadControl.CONE,       "lightUpBot",        cone,   State.INPUT},
+        {State.INPUT,      KeypadControl.CUBE,       "lightUpBot",        cube,   State.INPUT},
+        {State.INPUT,      KeypadControl.GRID_1,      null,               null,   State.PENDING1},
+        {State.INPUT,      KeypadControl.GRID_2,      null,               null,   State.PENDING1},
+        {State.INPUT,      KeypadControl.GRID_3,      null,               null,   State.PENDING1},
+        {State.PENDING1,   KeypadControl.LEFT,        null,               null,   State.PENDING2},
+        {State.PENDING1,   KeypadControl.MIDDLE,      null,               null,   State.PENDING2},
+        {State.PENDING1,   KeypadControl.RIGHT,       null,               null,   State.PENDING2},
+        {State.INPUT,      KeypadControl.CLEAR_ENTRY, "clearInputBuffer", none,   State.INPUT},
+        {State.PENDING2,   KeypadControl.CLEAR_ENTRY, "clearInputBuffer", none,   State.INPUT},
+        {State.PENDING2,   KeypadControl.SCORE_LOW,   "armJoystick",      null,   State.DONE},
+        {State.PENDING2,   KeypadControl.SCORE_MED,   "armJoystick",      null,   State.DONE},
+        {State.PENDING2,   KeypadControl.SCORE_HIGH,  "armJoystick",      null,   State.DONE},
     };
     
     void lightUpBot() {}
@@ -125,74 +143,100 @@ public class Keypad extends JDialog implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        System.out.println(keyCode);
+        // System.out.println(keyCode);
         if(keyCode == 144) {
         	picLabel.setIcon(lock);
-        	label.setText("Press NumLock; then press CLEAR.");
+        	feedbackLabel.setText("Press NumLock; then press CLEAR.");
         	return;
         }
+        feedbackLabel.setText("");
         if(KeypadControl.map.containsKey(keyCode)) {
         	KeypadControl control = KeypadControl.valueOf(keyCode);
         	String controlName = control.name();
         	switch(state) {
         	case INPUT:
             	switch(control) {
-            	case GET_CONE:
+            	case CONE:
             		picLabel.setIcon(cone);
             		inputBuffer = new StringBuffer(controlName);
             		break;
-            	case GET_CUBE:
+            	case CUBE:
             		picLabel.setIcon(cube);
             		inputBuffer = new StringBuffer(controlName);
             		break;
             	case CLEAR_ENTRY:
-            		picLabel.setIcon(none);
-            		inputBuffer = new StringBuffer("CLEAR");
-            		label.setText("");
+            		clear();
             		break;
-            	case CONE_LEFT:
-            	case CUBE_MIDDLE:
-            	case CONE_RIGHT:
+            	case GRID_1:
+            	case GRID_2:
+            	case GRID_3:
             		//
             		// TODO ERROR-CHECK HERE!!!
             		//      what if we have a cone and the driver presses CUBE_MIDDLE?
             		//
-            		inputBuffer = new StringBuffer(controlName);
-            		state = State.PENDING;
-            		break;
-            	default:
-            		break;
-            	}
-        		break;
-        	case PENDING:
-            	switch(control) {
-            	case DEPLOY_LOW: 
-            	case DEPLOY_MID: 
-            	case DEPLOY_HIGH:
             		inputBuffer.append("; " + controlName);
-            		state = State.INPUT;
-            		break;
-            	case CLEAR_ENTRY: picLabel.setIcon(none);
-            		inputBuffer = new StringBuffer("");
-            		label.setText("");
-            		state = State.INPUT;
-            		break;
-            	default:
-            		break;
+            		state = State.PENDING1;
             	}
-        		break;
+            	break;
+        	case PENDING1:
+            	switch(control) {
+            	case CLEAR_ENTRY:
+            		clear();
+            		break;
+            	case LEFT:
+            	case MIDDLE:
+            	case RIGHT:
+            		inputBuffer.append("; " + controlName);
+            		state = State.PENDING2;
+            	}
+            	break;
+           	case PENDING2:
+           		switch(control) {
+            	case CLEAR_ENTRY:
+            		clear();
+            		break;
+            	case SCORE_MED:
+            	case SCORE_HIGH:
+            		String buf = inputBuffer.toString().toLowerCase();
+            		if((buf.contains("cube") && buf.contains("middle")) ||
+            			buf.contains("cone") && !buf.contains("middle")) {
+            			// input is OK
+            		}
+            		else {
+            			feedbackLabel.setText("TRY AGAIN!");
+            			break;
+            		}
+            	case SCORE_LOW: 
+            		inputBuffer.append("; " + controlName);
+            		state = state.DONE;
+            	}
+           		break;
+           	case DONE:
+           		switch(control) {
+            	case CLEAR_ENTRY:
+            		clear();
+            		break;
+           		}
         	}
         	String command = inputBuffer.toString();
         	if(command.length() > 0) {
-		        label.setText(command);
-		        if(state != State.PENDING) {
+		        commandLabel.setText(command);
+		        if(state != State.PENDING1 && state != State.PENDING2) {
 			        table.putValue("driver entry", NetworkTableValue.makeString(command));
 			        System.out.println("sent " + command + " to network tables");
+            		state = State.INPUT;
 		        }
         	}
         }
 	}
 
+	private void clear() {
+		picLabel.setIcon(none);
+		inputBuffer = new StringBuffer("CLEAR");
+		commandLabel.setText("");
+		state = State.INPUT;
+	}
+	
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 	}
